27386e673e3d899fcd6ed30a3724ddd28d88f23a
SafronovA <alexey.safronov96@gmail.com>
2021-09-29 13:33:02 +0300

    Initial Commit

---
+++ iOSHomeTask2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSHomeTask2
+//
+//  Created by Aliaksei Safronau EPAM on 29.09.21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
