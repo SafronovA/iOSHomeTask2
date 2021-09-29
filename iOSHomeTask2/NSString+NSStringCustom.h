323a9e1a8f4746ed0b1e4be390f7eb3c7ab65229
SafronovA <alexey.safronov96@gmail.com>
2021-09-29 13:49:00 +0300

    Associated references mechanism + message forvarding

---
+++ iOSHomeTask2/NSString+NSStringCustom.h
@@ -0,0 +1,17 @@
+//
+//  NSString+NSStringCustom.h
+//  Hometask2
+//
+//  Created by Aliaksei Safronau EPAM on 27.09.21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (NSStringCustom)
+
+@property(nonatomic, strong) NSUUID *uuid;
+@end
+
+NS_ASSUME_NONNULL_END
